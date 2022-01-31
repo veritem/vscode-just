f644231efaf702a373714cc9f775dcd1b1f2b8f7
Lucas Schwiderski <lucas@lschwiderski.de>
2022-01-31 17:42:52 +0100

    chore: Upgrade dependencies

--- src/just/run-recipe.ts
+++ src/just/run-recipe.ts
@@ -1,37 +1,34 @@
-import * as execa from 'execa'
-import { ExecaError } from 'execa'
-import { EOL } from 'os'
-import { cwd } from 'process'
-import { Recipe, RunRecipeResult } from '../types'
+import { execa, ExecaError } from 'execa';
+import { Recipe, RunRecipeResult } from '../types';
 
 /**
  * Gets a list of commands you can run from a justfile.
  */
 export async function runRecipe(recipe: Recipe): Promise<RunRecipeResult> {
-  try {
-    // make the call to just
-    const execaResult = await execa('just', [recipe.name])
+    try {
+        // make the call to just
+        const execaResult = await execa('just', [recipe.name]);
 
-    // successful call to the executable?
-    if (execaResult.code === 0) {
-      // split up the result
-      return {
-        kind: 'ok',
-        stdout: execaResult.stdout && execaResult.stdout.trim(),
-      }
-    }
-  } catch (e) {
-    // runtime check for an execa error
-    if (e.cmd) {
-      const error = e as ExecaError
+        // successful call to the executable?
+        if (execaResult.exitCode === 0) {
+            // split up the result
+            return {
+                kind: 'ok',
+                stdout: execaResult.stdout && execaResult.stdout.trim(),
+            };
+        }
+    } catch (e) {
+        // runtime check for an execa error
+        if (e.cmd) {
+            const error = e as ExecaError;
 
-      return {
-        kind: 'error',
-        stderr: error.stderr && error.stderr.trim(),
-        stdout: error.stdout && error.stdout.trim(),
-      }
-    } else {
-      return { kind: 'unknown' }
+            return {
+                kind: 'error',
+                stderr: error.stderr && error.stderr.trim(),
+                stdout: error.stdout && error.stdout.trim(),
+            };
+        } else {
+            return { kind: 'unknown' };
+        }
     }
-  }
 }
